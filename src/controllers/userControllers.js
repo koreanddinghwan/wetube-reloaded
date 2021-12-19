9c382da2066933d137e9873a7a7401cf43c139b4
koreanddinghwan <myunghwan0421@gmail.com>
2021-12-19 14:16:02 +0900

    restart

--- src/controllers/userControllers.js
+++
@@ -1,9 +0,0 @@
-const Join = (req, res) => res.send("Join here");
-const EditUser = (req, res) => res.send("Edit user");
-const DeleteUser = (req, res) => res.send("Delete user");
-const Login = (req, res) => res.send("Login");
-const Logout = (req, res) => res.send("Logout");
-
-const See = (req, res) => res.send("See");
-
-export { EditUser, DeleteUser, Join, Login, See, Logout };
