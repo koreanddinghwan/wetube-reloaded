880612eb976f38ac5a0dcafe7716fed9b5f416e9
koreanddinghwan <myunghwan0421@gmail.com>
2022-01-03 13:56:38 +0900

    9.0.0 webpack

--- src/server.js
+++ src/server.js
@@ -36,6 +36,7 @@ app.use(
 
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/user", userRouter);
