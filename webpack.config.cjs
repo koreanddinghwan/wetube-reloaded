b546012c490fef129bc3472e107c3c224e945b1e
koreanddinghwan <myunghwan0421@gmail.com>
2022-01-06 13:52:55 +0900

    11.0

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,10 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   mode: "development",
   watch: true,
 
@@ -12,7 +15,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
