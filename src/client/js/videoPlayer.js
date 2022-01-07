e2a082c4d4bd43b04ddcdf1fd76a9c992c14d5ee
koreanddinghwan <myunghwan0421@gmail.com>
2022-01-07 10:35:22 +0900

    11.5

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,59 @@
-console.log("hi");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volumn");
+const video = document.querySelector("video");
+
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5; //볼륨값 저장변수
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMuteClick = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumnChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value; //전역변수업데이트
+  video.volume = value; //video의 실제 볼륨조절
+};
+
+const formatTime = (seconds) => {
+  return new Date(seconds * 1000).toISOString().substring(14, 19);
+};
+
+const handleLooadedMetaData = (e) => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = (e) => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumnChange);
+video.addEventListener("loadedmetadata", handleLooadedMetaData);
+video.addEventListener("timeupdate", handleTimeUpdate);
