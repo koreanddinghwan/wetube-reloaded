abd7462c5642cf4b1a41f5181f48b6afddca82b2
koreanddinghwan <myunghwan0421@gmail.com>
2022-01-16 21:50:17 +0900

    13.1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,15 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+      audio: false,
+      video: { width: 200, height: 100 },
     });
     console.log(stream);
+    video.srcObject = stream;
+    video.play();
   } catch (e) {
     console.log(e);
   }
