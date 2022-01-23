34395fd8378a58945ef20b43eabe9c9890d8e325
koreanddinghwan <myunghwan0421@gmail.com>
2022-01-24 04:35:22 +0900

    16.1

---
+++ assets/js/commentSection.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/client/js/commentSection.js":
+/*!*****************************************!*\
+  !*** ./src/client/js/commentSection.js ***!
+  \*****************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\nvar videoContainer = document.getElementById(\"videoContainer\");\nvar form = document.getElementById(\"commentForm\");\nvar textarea = form.querySelector(\"textarea\");\nvar btn = form.querySelector(\"button\");\n\nvar handleSubmit = function handleSubmit(e) {\n  e.preventDefault();\n  var text = textarea.value;\n  var video = videoContainer.dataset.id; //videocontainer의 dataset프로퍼티이용\n};\n\nform.addEventListener(\"submit\", handleSubmit);\n\n//# sourceURL=webpack://wetube/./src/client/js/commentSection.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/client/js/commentSection.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,14 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const BASE_JS_PATH = "./src/client/js";
+
 module.exports = {
   entry: {
-    main: "./src/client/js/main.js",
-    videoPlayer: "./src/client/js/videoPlayer.js",
-    recorder: "./src/client/js/recorder.js",
+    main: `${BASE_JS_PATH}/main.js`,
+    videoPlayer: `${BASE_JS_PATH}/videoPlayer.js`,
+    recorder: `${BASE_JS_PATH}/recorder.js`,
+    commentSection: `${BASE_JS_PATH}/commentSection.js`,
   },
   mode: "development",
   watch: true,
